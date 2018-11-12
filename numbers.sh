bc822650658c18484c4b5c4bfd21020e3548db61
Dan Haub <haub@chapman.edu>
2018-11-12 10:49:33 -0800

    Adds script

--- numbers.sh
+++ numbers.sh
@@ -1,1 +1,24 @@
+# /bin/bash
+# numbers.sh
+# Dan Haub
 
+echo "Please enter a positive number"
+
+read NUM
+
+echo "Your number is:  "$NUM
+
+let N=1
+
+while [ "$N" -le "$NUM" ]
+do
+	if [ $((N%2)) -eq 0 ]
+	then
+		echo $N" Even"	
+	fi
+	if [ $((N%2)) -eq 1 ]
+	then
+		echo $N" Odd"
+	fi
+	N=$((N+1))
+done
